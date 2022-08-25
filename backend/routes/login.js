6bf5d779d72ab5d51426f63ea1cf3a3422742607
joannax <joannax@student.unimelb.edu.au>
2022-08-25 20:05:34 +1000

    fix file structure

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({mssg: 'HOME'});
-});
-
 router.get('/login', (req, res) => {
     res.json({mssg: 'login'});
 });
