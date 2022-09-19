1963192e2f9c0e880367f45e285fd62f3e4d121d
Elizabeth Wong <elizabeth.wong@student.unimelb.edu.au>
2022-09-19 22:31:08 +1000

    fixed signup button links

--- frontend/Website-w-links/components/SignUpButton/index.jsx
+++ frontend/Website-w-links/components/SignUpButton/index.jsx
@@ -4,14 +4,17 @@ import "antd/dist/antd.variable.css";
 import * as AntDesignIcons from "@ant-design/icons";
 import * as Antd from "antd";
 import "./SignUpButton.css";
+import { Link } from "react-router-dom";
 
 function SignUpButton() {
   return (
+  <Link to="/sign-up-page">
     <div className="sign-up-button">
       <Antd.Button size="large" type="default" shape="default" disabled={false}>
         SIGN UP
       </Antd.Button>
     </div>
+  </Link>
   );
 }
 
