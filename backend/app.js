8a31e49c5191cdf3f40207c8639ade2a7e8ee2ed
Harold Liu <haroldfl@outlook.com>
2022-09-20 22:09:30 +1000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,14 +18,11 @@ const { applySpec } = require('ramda');
 const app = express();
 
 // middleware
-app.prepare().then(() => {
-    const server = express();
-
-    server.use(function(req, res, next) {
+app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-    });
+};
 app.use(express.json());
 app.use(
     express.urlencoded({ extended: true })
