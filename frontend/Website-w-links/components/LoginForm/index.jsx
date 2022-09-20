a749883732788e474751db50cd1758a750fb96f6
Elizabeth Wong <elizabeth.wong@student.unimelb.edu.au>
2022-09-20 23:17:15 +1000

    need to fix register

--- frontend/Website-w-links/components/LoginForm/index.jsx
+++ frontend/Website-w-links/components/LoginForm/index.jsx
@@ -4,7 +4,6 @@ import "antd/dist/antd.variable.css";
 import * as AntDesignIcons from "@ant-design/icons";
 import * as Antd from "antd";
 import "./LoginForm.css";
-
 import axios from "../../api/axios";
 
 // to collect the input data 
@@ -24,12 +23,13 @@ const LoginForm = () => {
     });
   }
 
+  // 
   async function PostLogin(){
   axios.post('/login', {
     email: values.email,
     password: values.password,
   })
-  .then(function (response) {
+  .then(function (response){
     console.log(response);
   })
   .catch(function (error) {
@@ -107,67 +107,3 @@ const LoginForm = () => {
 
 export default LoginForm;
 
-/*
-function LoginForm() {
-  return (
-    <div className="login-form">
-      <Antd.Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        style={{
-          maxWidth: "500px",
-          "--ant-primary-color": "#1890ff",
-          "--ant-primary-color-hover": "#40a9ff",
-        }}
-      >
-        <Antd.Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please fill username & password",
-            },
-          ]}
-        >
-          <Antd.Input placeholder="Username" prefix={<AntDesignIcons.UserOutlined className="site-form-item-icon" />} />
-        </Antd.Form.Item>
-        <Antd.Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please fill username & password",
-            },
-          ]}
-        >
-          <Antd.Input
-            type="password"
-            placeholder="Password"
-            prefix={<AntDesignIcons.LockOutlined className="site-form-item-icon" />}
-          />
-        </Antd.Form.Item>
-        <Antd.Form.Item>
-          <Antd.Form.Item noStyle name="remember" valuePropName="checked">
-            <Antd.Checkbox>Remember me</Antd.Checkbox>
-          </Antd.Form.Item>
-
-          <a href="javascript:void(0)" style={{ float: "right" }} className="login-form-forgot">
-            Forgot password
-          </a>
-        </Antd.Form.Item>
-        <Antd.Form.Item>
-          <Antd.Button type="primary" htmlType="submit" style={{ width: "100%" }} className="login-form-button">
-            Log in
-          </Antd.Button>
-          Or <a href="/sign-up-page">register now!</a>
-        </Antd.Form.Item>
-      </Antd.Form>
-    </div>
-  );
-}
-
-export default LoginForm;
-*/
--- frontend/Website-w-links/components/SignUpPage/index.jsx
+++ frontend/Website-w-links/components/SignUpPage/index.jsx
@@ -1,6 +1,97 @@
 import React from "react";
+import React, { useState } from "react";
 import "./SignUpPage.css";
+import axios from "../../api/axios";
+
+const SignUpPage = () => {
+    const [values, setValues] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+
+      });
+
+
+    async function PostSignUp(){
+    axios.post('/register', {
+        firstName: "",
+        lastName: "",
+        email: values.email,
+        password: values.password,
+    })
+    .then(function (response){
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    }
+    
+    return (
+
+    <div className="form">
+    <form className="signpage">
+        <label className="sign">
+            Sign Up
+        </label>
+        <label className="slogan">
+          and start scrolling through the marketplace!
+        </label>
+        <div className="mb-3">
+            <label> First Name</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="First Name"
+                onChange={(e)=> setValues({...values, firstName:e.target.value})}
+            />
+        </div>
+        <div className="mb-3">
+            <label> Last Name</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Last Name"
+                onChange={(e)=> setValues({...values, lastName:e.target.value})}
+            />
+        </div>
+        <div className="mb-3">
+            <label> Email Address</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(e)=> setValues({...values, email:e.target.value})}
+            />
+        </div>
+        <div className="mb-3">
+            <label> Password</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Password"
+                onChange={(e)=> setValues({...values, password:e.target.value})}
+            />
+        </div>
+        <div className="d-grid">
+            <button type="submit" className="btn btn-success" onClick={PostSignUp()}>
+                Sign Up
+            </button>
+        </div>
+        <p className="forgot-password text-right">
+            Already Registered <a href="/login-page"> sign in?</a>
+        </p>
+    </form>
+  </div>
+  );
+}
+
+export default SignUpPage;
+
+/*
 function SignUpPage() {
+
   return (
     <div className="form">
     <form className="signpage">
@@ -56,3 +147,4 @@ function SignUpPage() {
 }
 
 export default SignUpPage;
+*/
