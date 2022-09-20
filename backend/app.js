e091ad172d46b1b6664d93e2223d6fa931db43ba
Harold Liu <haroldfl@outlook.com>
2022-09-20 22:10:32 +1000

    Added missing bracket

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-};
+});
 app.use(express.json());
 app.use(
     express.urlencoded({ extended: true })
