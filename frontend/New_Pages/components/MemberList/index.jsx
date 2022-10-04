649255eee620ca90ad8698b193afef31d5c0e951
xiaolan-wang <xiaolanw2@student.unimelb.edu.au>
2022-10-05 00:33:03 +1100

    fix member list component & member list page by usEffect

--- frontend/New_Pages/components/MemberList/index.jsx
+++ frontend/New_Pages/components/MemberList/index.jsx
@@ -1,27 +1,74 @@
-import React, { Component } from 'react'
-import './MemberList.css'
 
-export default class MemberList extends Component {
-	render() {
-		const {users,isFirst,isLoading,err} = this.props
-		return (
-			<div className="row">
-				{
-					isFirst ? <h2>search to start...</h2> :
-					isLoading ? <h2>Loading......</h2> :
-					err ? <h2 style={{color:'red'}}>{err}</h2> :
-					users.map((userObj)=>{
-						return (
-							<div key={userObj.id} className="card">
-								<a rel="noreferrer" href={"?userObj.html_url"} target="_blank">
-									<img alt="head_portrait" src={"?userObj.avatar_url"} style={{width:'100px'}}/>
-								</a>
-								<p className="card-text">{"?userObj.name"}</p>
-							</div>
-						)
-					})
-				}
+import logo from "../../dist/img/t34-logo.jpg";
+import React, {useEffect, useState}from "react";
+import axios from "../../api/axios";
+function MemberList() {
+
+
+    const [posts, setPosts] = useState([]);
+
+    // Define the function that fetches the data from API
+    const fetchData = async () => {
+      const { data } = await axios.get("/public");
+      setPosts(data);
+    };
+  
+    // Trigger the fetchData after the initial render by using the useEffect hook
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+
+return(
+    <div className="products-wrapper">  
+    {posts.map((post) => {
+      return(
+        <div className="products-wrapper-test">  
+        {/* products display 1st row*/} 
+        <div className="wrapper" >
+        <div class="row2">
+          <div class="column">
+          <div class="card">
+            {/*  add href to member page*/}
+            <a href="/member-list-page" >
+            <div className="img-wrap"> 
+              <img src={logo} className="logo-position">
+              </img> 
+            </div>
+
+			{/* spacer instead of wishlist btn*/}
+            &nbsp;
+			<div className="item-cart">
+				<h5>{post.user_id}</h5>
+				{/* member list dropdown */}
+				<div className="move-drop-btn">
+					<div class="dropdown">
+						<button class="dropbtn">Sort by: Default</button>
+						<div class="dropdown-content">
+							<a href="#">remove member</a>
+							<a href="#">set as admin</a>       
+						</div>
+				    </div>
+			    </div> 
 			</div>
-		)
-	}
+             
+             
+            </a>
+          </div>
+          </div>   
+          
+        </div>
+        </div>
+    
+    
+        </div>  
+      )
+    })}
+
+
+    </div>  
+      
+);
 }
+
+export default MemberList;
--- frontend/New_Pages/components/MemberListPage/index.jsx
+++ frontend/New_Pages/components/MemberListPage/index.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
+import "./MemberListPage.css";
 import logo from "../../dist/img/t34-logo.jpg";
 import axios from "../../api/axios";
-import "./MemberListPage.css";
-import MemberList from '../MemberList'
-
+import MemberList from "../MemberList";
 /* icon imports */
 import {AiOutlineHome} from 'react-icons/ai';
 import {HiOutlineShoppingBag} from 'react-icons/hi';
@@ -11,23 +10,24 @@ import {MdOutlineGroups} from 'react-icons/md';
 import {AiOutlineUsergroupAdd} from 'react-icons/ai';
 import {TbStar} from 'react-icons/tb';
 import {AiOutlineLock} from 'react-icons/ai';
-import {RiBookOpenLine, RiGenderlessFill} from 'react-icons/ri';
-
-
+import {RiBookOpenLine} from 'react-icons/ri';
 
 const MemberListPage = () => {
 
-state = { //初始化状态
-    users:[], //users初始值为数组
-    isFirst:true, //是否为第一次打开页面
-    isLoading:false,//标识是否处于加载中
-    err:'',//存储请求相关的错误信息
-} 
 
-//更新App的state
-updateAppState = (stateObj)=>{
-    this.setState(stateObj)
-}
+  const [posts, setPosts] = useState([]);
+
+  // Define the function that fetches the data from API
+  const fetchData = async () => {
+    const { data } = await axios.get("/public");
+    setPosts(data);
+  };
+
+  // Trigger the fetchData after the initial render by using the useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
 return (
     <div className="parent" >
      {/* top nav bar*/}
@@ -36,7 +36,7 @@ return (
         <a href="/home-page"> <AiOutlineHome className="icon"/> Home</a>
         <a href="/sell-page"> <HiOutlineShoppingBag className="icon"/> Sell</a>
         <a href="/group-page"> <AiOutlineUsergroupAdd className="icon"/> Groups</a>
-        <a class="active" href="/my-group-page"> <MdOutlineGroups className="icon"/> My Groups</a>
+        <a href="/my-group-page"> <MdOutlineGroups className="icon"/> My Groups</a>
         <a href="/wishlist-page"> <TbStar className="icon"/> Wishlist</a>
       <div class="nav-login">
       {/* search bar*/}
@@ -54,7 +54,7 @@ return (
     </div>
     <hr />
     <div className="number-listings"> 123 members</div>
-    {/* search member button*/} 
+    {/* search member button (need fix)*/} 
     <div className = "search-member">
         <input type="text"placeholder="Search Member.."> 
             </input>
@@ -66,29 +66,16 @@ return (
     <div class="left-box">
         <div className="square-pic">  
         <div className="img-wrap"> <img src={logo} className="logo-position"></img> </div>
-
-
         </div> 
     </div>
     
     {/* member list of the group*/}
-    <div className = "member-list">
-    <div className="card">
-                <img alt="head_portrait" src={logo} style={{width:'100px'}}/>
-								<p className="card-text">member name</p>
-                <div className="member-manage-drop-btn">
-                  <div class="dropdown">
-                    <button class="dropbtn">manage</button>
-                    <div class="dropdown-content">
-                        <a href="#"> move member</a>
-                        <a href="#"> set as admin</a>       
-                    </div>
-                  </div>
-                </div>
-							</div>
-        
-            <MemberList/>
-
+    <div className="wrapper" >
+        <div class="row2">
+          <div class="column">
+          <MemberList/>
+          </div>
+        </div>
     </div>
 
     {/* next page bar here*/}
