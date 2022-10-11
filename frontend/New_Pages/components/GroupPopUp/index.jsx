384dbd6b490c7a35656bbe60b148dc25baa1b131
Elizabeth Wong <elizabeth.wong@student.unimelb.edu.au>
2022-10-11 17:01:40 +1100

    group detail popup

--- frontend/New_Pages/components/GroupPopUp/index.jsx
+++ frontend/New_Pages/components/GroupPopUp/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./GroupPopUp.css";
+
+import logo from "../../dist/img/t34-logo.jpg";
 const Popup = props => {
 
 
@@ -8,21 +10,24 @@ const Popup = props => {
     <div className="popup-box">
       <div className="box">
         <span className="close-icon" onClick={props.handleClose}>x</span>
-        GROUP NAME: XXX 
+        <div className="square-popup">
+          <img src={logo} className="popup-img"></img> 
+        </div>
+        <div className="popup-text"> 
+        Marketplace Sellers <p> 31k Members</p>
+       
+        <button> Member's List</button>
+       
         <br/>
-        GROUP DESCRIPTION: 123
-        <br/>
-        join group btn
-        <br/>
-        MEMBERS AMOUNT: 123
-        <br/>
-        MEMBERS LIST BTN 
-        <br/>
-        owner
-
-
-      </div>
+        Owner: YenFug</div>
+        <div className="header-popup">Marketplace Sellers </div> 
+        <hr className="hr-line"/>
+        <div className="test"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare lectus sit amet est placerat in egestas erat. Duis at tellus at urna condimentum mattis pellentesque id nibh. Vulputate eu scelerisque felis imperdiet proin. Varius quam quisque id diam vel quam elementum. Felis imperdiet proin fermentum leo vel orci porta. Consequat mauris nunc congue nisi vitae suscipit tellus mauris. Sed euismod nisi porta lorem. Lacus sed viverra tellus in hac habitasse platea dictumst. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. In hendrerit gravida rutrum quisque non tellus orci ac. Molestie nunc non blandit massa enim nec dui nunc. At varius vel pharetra vel turpis nunc eget lorem dolor. Quis eleifend quam adipiscing vitae proin. Tellus at urna condimentum mattis pellentesque id nibh tortor. </div>
+        
+        <div className="popup-btn"> <button > Join Group</button> </div>
     </div>
+    </div>
+    
   );
 };
  
