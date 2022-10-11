e01a1a1043278472cf7fed3f52b5e2c4d2189951
Elizabeth Wong <elizabeth.wong@student.unimelb.edu.au>
2022-10-11 16:15:19 +1100

    try to fix cors policy

--- backend/app.js
+++ backend/app.js
@@ -76,6 +76,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
 });
 
 
