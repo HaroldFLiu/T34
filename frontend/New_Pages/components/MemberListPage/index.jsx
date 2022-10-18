c7ea067d98cbd244e3d2ad7c493acff839ab9ef0
joannax <joannax@student.unimelb.edu.au>
2022-10-18 16:15:07 +1100

    fix fetch members

--- frontend/New_Pages/components/MemberListPage/index.jsx
+++ frontend/New_Pages/components/MemberListPage/index.jsx
@@ -17,9 +17,7 @@ import {AiOutlineLock} from 'react-icons/ai';
 import {RiBookOpenLine} from 'react-icons/ri';
 
 const MemberListPage = () => {
-
-
-
+  
   {/* TO GET SINGLE ITEM NEED CONDITION TO ACCESS CLICKED ITEMS'S ID*/}
   const {groupId} = useParams()
   //const thisProduct = posts.find(prod => prod.id == productId)
@@ -28,93 +26,89 @@ const MemberListPage = () => {
 
   {/*fetch item data*/}
   
-  const [groups, setGroups] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [admins, setAdmins] = useState([]);
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [firstRender, setFirstRender] = useState(false);
 
   const [currentPage, setCurrentPage] = useState(1);
   // 10 items displayed per page
   const [recordsPerPage] = useState(10);
 
-  useEffect(() => {
+  const fetchData = async () => {
     axios.get('/groups/'+groupId+'/members')
-        .then(res => {
-          console.log(res);
-                setData(res.data.members);
-                setLoading(false);
-                
-            })
-            .catch((error) => {
-              console.log(error);
-                alert('There was an error while retrieving the data');
-            })
-            .then(fetchData())
-}, [])
-console.log(data);
+    .then(res => {
+      console.log(res);
+      setMembers(res.data.members);
+      setAdmins(res.data.admins);
+      setFirstRender(true);
+
+      console.log(members);
+      console.log(admins);
+    })
+    .catch((error) => {
+      console.log(error);
+        alert('There was an error while retrieving the data');
+    })
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [firstRender]);
+
 const indexOfLastRecord = currentPage * recordsPerPage;
 const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
 //const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
-const currentRecords = ["aaa"]; //!need fix
-const nPages = Math.ceil(data.length / recordsPerPage)
+//const currentRecords = ["aaa"]; //!need fix
+//const nPages = Math.ceil(data.length / recordsPerPage)
 
-const fetchGroups = async () => {
-  const { data } = await axios.get(`/groups/${groupId}/members`);
-  setGroups(data);
-};
-
-
-useEffect(() => {
-  fetchGroups();
-}, []);
-
-
-  {/*get user id axios.get(BASE_URL + '/todos', { withCredentials: true });*/}
-  var coookie = new Cookie();
-  const [user, setUser] = useState([]);
-  const fetchData = async () => {
-    const server_res = await axios.get("/getuser", {withCredentials:true, headers:{'Authorization':coookie.get("token")}});
-    console.log(server_res);
-    //const user = server_res.data.user_email;
-    const user = server_res.data;
-    setUser(user);
-    //console.log(server_res.data.user_id);
+  // {/*get user id axios.get(BASE_URL + '/todos', { withCredentials: true });*/}
+  // var coookie = new Cookie();
+  // const [user, setUser] = useState([]);
+  // const fetchData = async () => {
+  //   const server_res = await axios.get("/getuser", {withCredentials:true, headers:{'Authorization':coookie.get("token")}});
+  //   console.log(server_res);
+  //   //const user = server_res.data.user_email;
+  //   const user = server_res.data;
+  //   setUser(user);
+  //   //console.log(server_res.data.user_id);
   
-  };
+  // };
   
 
-  {/*method to unpack the data and fetch effect*/ }
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // {/*method to unpack the data and fetch effect*/ }
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-   // log OUT HERE
-   const handleLogOut = async () => {
-    await axios.put("/logout", {} ,{withCredentials:true, headers:{'Authorization':coookie.get("token")}})
-    .then(response => {
-      if (response.status === 200) {
-        location.pathname='/login-page';
-      }
-    })
-    .catch(error => {
-      console.log("Error signing out", error);
-    });
-  };
+  //  // log OUT HERE
+  //  const handleLogOut = async () => {
+  //   await axios.put("/logout", {} ,{withCredentials:true, headers:{'Authorization':coookie.get("token")}})
+  //   .then(response => {
+  //     if (response.status === 200) {
+  //       location.pathname='/login-page';
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.log("Error signing out", error);
+  //   });
+  // };
 
-  //search
-  const [searchInput, setSearchInput] = useState('');
-  const [filteredResults, setFilteredResults] = useState([]);
-  const searchItems = (searchValue) => {
-    setSearchInput(searchValue)
-    if (searchInput !== '') {
-        const filteredData = data.filter((item) => {
-            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-        })
-        setFilteredResults(filteredData)
-    }
-    else{
-        setFilteredResults(data)
-    }
-}
+  // //search
+  // const [searchInput, setSearchInput] = useState('');
+  // const [filteredResults, setFilteredResults] = useState([]);
+  // const searchItems = (searchValue) => {
+  //   setSearchInput(searchValue)
+  //   if (searchInput !== '') {
+  //       const filteredData = data.filter((item) => {
+  //           return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+  //       })
+  //       setFilteredResults(filteredData)
+  //   }
+  //   else{
+  //       setFilteredResults(data)
+  //   }
+  //}
 
 return (
     <div className="parent" >
@@ -122,15 +116,12 @@ return (
     <div class="navbar">
     <h1 className="website-title"> Market34</h1>
         <a href="/home-page"> <AiOutlineHome className="icon"/> Home</a>
-        <Link to={`/sell-page/${user.user_id}`}> Sell </Link>
         <a href="/group-page"> <AiOutlineUsergroupAdd className="icon"/> Groups</a>
         <a href="/my-group-page"> <MdOutlineGroups className="icon"/> My Groups</a>
         <a href="/wishlist-page"> <TbStar className="icon"/> Wishlist</a>
       <div class="nav-login">
       {/* search bar*/}
       {/* need to add logout btn, rn just redirects without sign out*/}
-      <a href="#"> <button onClick={() => handleLogOut()}> <AiOutlineLock className="icon"/> Log Out </button></a>
-      <a href="#"><RiBookOpenLine className="icon" /> Welcome: {user.first}</a>
       <a href="/checkout-page"> Cart</a>
    
       <input type="text"placeholder="Search.."> 
@@ -143,19 +134,19 @@ return (
       
     </div>
     <hr />
-    <div className="number-listings"> {data.length} members</div>
+    {/* <div className="number-listings"> {data.length} members</div> */}
     {/* search member button (need fix)*/} 
-    <div className = "search-member">
+    {/* <div className = "search-member">
         <input type="text"placeholder="Search Member.." onChange={(e) => searchItems(e.target.value)}> 
             </input>
-    </div>
+    </div> */}
     
     <hr />
 
        {/*Group img*/}    
     <div class="left-box">
         <div className="square-pic">  
-        <div className="img-wrap"> <img src={groups.image_urls} className="popup-img"></img> </div>
+        {/* <div className="img-wrap"> <img src={groups.image_urls} className="popup-img"></img> </div> */}
         </div> 
     </div>
     
@@ -165,20 +156,15 @@ return (
           <div class="column">
             {/* {searchInput.length > 1 ?
             (filteredResults.map((item) => {return }))} */}
-          <MemberList data={currentRecords}/>
+          {/* <MemberList data={currentRecords}/>
           <PageNext
                 nPages={nPages}
                 currentPage={currentPage}
                 setCurrentPage={setCurrentPage}
-            />
+            /> */}
           </div>
         </div>
-    </div>
-
-
-    
-
-       
+    </div> 
 </div>
 
     );
