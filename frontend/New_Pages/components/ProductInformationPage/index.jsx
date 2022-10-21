a2ed8b61af5ab39c5f8713099c6b89f5fa9492fe
joannax <joannax@student.unimelb.edu.au>
2022-10-21 20:38:04 +1100

    fix bug

--- frontend/New_Pages/components/ProductInformationPage/index.jsx
+++ frontend/New_Pages/components/ProductInformationPage/index.jsx
@@ -19,6 +19,7 @@ const ProductInformationPage = () => {
   const [item, setItems] = useState({});
   const [seller, setSeller] = useState({});
   const [buyer, setBuyer] = useState({});
+  const [isBought, setIsBought] = useState(false);
   const [isSeller, setIsSeller] = useState(false);
   const [category, setCategory] = useState({});
 
@@ -50,6 +51,7 @@ const ProductInformationPage = () => {
 
       if (res.data.buyer) {
         setBuyer(res.data.buyer);
+        setIsBought(true);
       }
     })
     .catch(() => {
@@ -131,7 +133,7 @@ const ProductInformationPage = () => {
           <div className="info-text-centered-price"> <b>${item.price}</b></div>
           <hr />
           <div className="info-text-left"> <AiFillTag /><b>Tags: </b> {category.name} </div>
-          {buyer && <div className="info-text-left"> <br/><b>Sold To: </b> {buyer.first_name} {buyer.last_name} </div>}
+          {isBought && <div className="info-text-left"> <br/><b>Sold To: </b> {buyer.first_name} {buyer.last_name} </div>}
 
           {/* different dislay depending on if you are seller or not, as well as if item is in cart or not*/}
           {!added && !isSeller && <button className="purchase-btn" onClick={() => addToCart()}> ADD TO CART </button>}
