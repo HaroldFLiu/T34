292bb1d21969c383868611ef3f37315fd4772414
Harold Liu <haroldfl@outlook.com>
2022-10-23 14:35:20 +1100

    Update axios.js

--- frontend/New_Pages/api/axios.js
+++ frontend/New_Pages/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: 'http://market34-back.onrender.com',
+   baseURL: 'market34.up.railway.app',
    //baseURL: 'http://127.0.0.1:3000',
    withCredentials: true,
    xsrfHeaderName : "X-CSRFToken",
